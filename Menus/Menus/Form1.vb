b3b2017a036f6d65756129ff0033d1768463f2d1
ajpug@22 <ajpug138@learner.dvusd.org>
2024-01-08 12:25:21 -0700

    classwork(1/5/24)

---
+++ Menus/Menus/Form1.vb
@@ -0,0 +1,7 @@
+﻿Public Class Form1
+    Private Sub Button1_MouseClick(sender As Object, e As MouseEventArgs) Handles Button1.MouseClick
+        If Button Then
+
+        End If
+    End Sub
+End Class
