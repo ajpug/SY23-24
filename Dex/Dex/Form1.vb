ba84e457136a154c6ed67bc92441ada33fcbf12b
ajpug@22 <ajpug138@learner.dvusd.org>
2024-01-10 12:56:40 -0700

    Class again
    1/10/24

---
+++ Dex/Dex/Form1.vb
@@ -0,0 +1,12 @@
+﻿Public Class Form1
+    Private Sub SaveToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SaveToolStripMenuItem.Click
+        Dim outfile As New IO.StreamWriter("out.txt")
+        outfile.WriteLine(TextBox1.Text)
+        outfile.Close()
+    End Sub
+
+    Private Sub NewToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NewToolStripMenuItem.Click
+        TextBox1.Clear()
+
+    End Sub
+End Class
